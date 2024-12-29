1bb89cbe584a28af34fbef74885863fb5d355a50
Hammad Khan <hammadk42623@gmail.com>
2024-12-29 18:58:58 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className='text-6xl'>Testing</h1>
+    </>
+  )
+}
+
+export default App
